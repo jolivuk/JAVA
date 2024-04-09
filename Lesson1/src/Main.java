3d0aec673111155d4f2c78dd8fec6db07553e409
Nina Janeckova <160600083+jolivuk@users.noreply.github.com>
2024-04-09 09:43:51 +0200

    Homework1

---
+++ Lesson1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+//        Первый вариант
+        System.out.println("Hello,");
+        System.out.println("my");
+        System.out.println("name");
+        System.out.println("is");
+        System.out.println("!");
+
+//        Второй вариант
+        System.out.print("Hello, ");
+        System.out.print("my ");
+        System.out.print("name ");
+        System.out.print("is ");
+        System.out.print("!");
+    }
+}
